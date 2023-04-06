a4633e6c193196e2c1e43d854d2c8c6ca0f0a8ba
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-06 13:27:37 +0900

    Create StringOperation.java

---
+++ Data_and_operation/StringOperation.java
@@ -0,0 +1,10 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World".length()); //11 (include gap)
+		System.out.println("Hello, [[name]] ... bye.".replace("[[name]]", "dangdang"));
+	}
+
+}
