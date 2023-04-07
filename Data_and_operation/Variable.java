7a095a7495af4fbe6865e7c15f8e4da3dae28327
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-07 14:48:16 +0900

    Create Variable.java

---
+++ Data_and_operation/Variable.java
@@ -0,0 +1,16 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;//number->integer
+		System.out.println(a);
+		
+		double b=1.1; //real number -> double ... -2.0, -1.0 etc
+		System.out.println(b);
+		
+		String c="Hello World";
+		System.out.println(c);
+	}
+
+}
