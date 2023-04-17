5d37941c581be1260d4acc55fd02fdbe25e7c21d
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-17 11:30:24 +0900

    Create ExceptionApp.java

---
+++ ExceptionApp/ExceptionApp.java
@@ -0,0 +1,24 @@
+
+public class ExceptionApp {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+		int[] scores= {10,20,30};
+
+		try {
+			System.out.println(2);
+			System.out.println(scores[3]);
+			System.out.println(3);
+//			System.out.println(2/0); 
+			System.out.println(4);
+		}catch(ArithmeticException e) {
+			System.out.println("wrong Calculation");
+		}
+		catch(Exception e) {
+			System.out.println("Something Wrong.");
+		}
+		
+		System.out.println(5);
+	}
+
+}
