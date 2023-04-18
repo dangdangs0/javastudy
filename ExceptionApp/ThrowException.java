406f52bde8242f184802eb986eef22aecd66054f
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-18 11:52:16 +0900

    Create ThrowException.java

---
+++ ExceptionApp/ThrowException.java
@@ -0,0 +1,11 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ThrowException {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		FileWriter f=new FileWriter("./data.txt");
+	}
+
+}
