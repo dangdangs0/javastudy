9bfb1f8eb0bde74cb3ca3b536f7bb429ea591200
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-19 12:13:54 +0900

    Create WhyMethod.java

---
+++ ProjectMethod/WhyMethod.java
@@ -0,0 +1,18 @@
+
+public class WhyMethod {
+	public static void printTwoTimesA() {
+		System.out.println("-");
+		System.out.println("A");
+		System.out.println("A");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		printTwoTimesA();
+		
+		printTwoTimesA();
+		
+		printTwoTimesA();
+	}
+
+}
