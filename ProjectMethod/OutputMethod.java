b06fb33d93fe3d125dd3b583b2cf02d2b7b67f8d
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-19 12:44:05 +0900

    Create OutputMethod.java

---
+++ ProjectMethod/OutputMethod.java
@@ -0,0 +1,17 @@
+
+public class OutputMethod {
+
+	public static String a() {
+		//..
+		return "a";
+	}
+	public static int one() {
+		return 1;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(a());
+		System.out.println(one());
+	}
+
+}
