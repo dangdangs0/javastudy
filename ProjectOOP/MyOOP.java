a1f310f2abdbc732aa4f8213c794d5e792fc420c
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-21 12:26:12 +0900

    Update MyOOP.java

--- ProjectOOP/MyOOP.java
+++ ProjectOOP/MyOOP.java
@@ -1,17 +1,26 @@
 public class MyOOP {
 	
 	public static void main(String[] args) {
-		Print.delimiter="----";
-		Print.A();
-		Print.A();
-		Print.B();
-		Print.B();
 		
-		Print.delimiter="****";
-		Print.A();
-		Print.A();
-		Print.B();
-		Print.B();
+		Print p1=new Print();
+		p1.delimiter="----"; //instance's delimiter
+		p1.A();
+		p1.A();
+		p1.B();
+		p1.B();
+		
+		Print p2=new Print();
+		p2.delimiter="****"; //instance's delimiter
+		p2.A();
+		p2.A();
+		p2.B();
+		p2.B();
+		
+		
+		p1.A();
+		p2.A();
+		p1.A();
+		p2.A();
 	}
 	
 
