033e58b7dadd8170de2175d202ce9591d20d14c3
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-23 13:57:31 +0900

    Update InheritanceApp.java

--- Inheritance/InheritanceApp.java
+++ Inheritance/InheritanceApp.java
@@ -1,44 +1,33 @@
 class Cal{
-	public int sum(int v1, int v2) {
-		return v1+v2;
+	int v1,v2;
+	Cal(int v1, int v2){
+		System.out.println("Cal init!!!");
+		this.v1=v1;
+		this.v2=v2;
 	}
-	public int sum(int v1, int v2, int v3) {//overloading
-		return this.sum(v1,v2)+v3;//sum(v1,v2)+v3 is ok.
+	public int sum() {
+		return this.v1+this.v2;
 	}
 }
 
 class Cal3 extends Cal{
-	public int sum(int v1, int v2) { //overriding
-		System.out.println("Cal3!!");
-		return super.sum(v1,v2);
+
+	Cal3(int v1, int v2) {
+		super(v1, v2);
+		System.out.println("Cal3 init!!");
 	}
-	
-	public int minus(int v1, int v2) {
-		return v1-v2;
+	public int minus() {
+		return this.v1-this.v2;
 	}
-	
 }
 
 public class InheritanceApp {
 
 	public static void main(String[] args) {
-		Cal c=new Cal();
-		System.out.println(c.sum(2,1));
-		System.out.println(c.sum(2,1,1));
-		
-		Cal3 c3=new Cal3();
-		System.out.println(c3.sum(2, 1));
-		System.out.println(c3.minus(2, 1));
-		System.out.println(c3.sum(2, 1));
+		Cal c=new Cal(2,1);
+		Cal3 c3=new Cal3(2,1);
+		System.out.println(c3.sum()); //3
+		System.out.println(c3.minus()); //1
 	}
 
-}
-
-class Cal2{
-	public int sum(int v1, int v2) {
-		return v1+v2;
-	}
-	public int minus(int v1, int v2) {
-		return v1-v2;
-	}
 }
