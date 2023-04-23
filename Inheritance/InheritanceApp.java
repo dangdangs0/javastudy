7fba236fceab0b37718a83165de967c45eed7559
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-23 12:39:16 +0900

    Update InheritanceApp.java

--- Inheritance/InheritanceApp.java
+++ Inheritance/InheritanceApp.java
@@ -2,9 +2,20 @@ class Cal{
 	public int sum(int v1, int v2) {
 		return v1+v2;
 	}
+	public int sum(int v1, int v2, int v3) {//overloading
+		return v1+v2+v3;
+	}
 }
 
 class Cal3 extends Cal{
+	public int sum(int v1, int v2) { //overriding
+		System.out.println("Cal3!!");
+		return v1+v2;
+	}
+	
+	public int minus(int v1, int v2) {
+		return v1-v2;
+	}
 	
 }
 
@@ -13,8 +24,11 @@ public class InheritanceApp {
 	public static void main(String[] args) {
 		Cal c=new Cal();
 		System.out.println(c.sum(2,1));
+		System.out.println(c.sum(2,1,1));
 		
-		Cal c3=new Cal3();
+		Cal3 c3=new Cal3();
+		System.out.println(c3.sum(2, 1));
+		System.out.println(c3.minus(2, 1));
 		System.out.println(c3.sum(2, 1));
 	}
 
