f820d07c5bf663fcc23f18187b921d8ee2a283ab
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-24 12:07:55 +0900

    Update InterfaceApp.java

--- Interface/InterfaceApp.java
+++ Interface/InterfaceApp.java
@@ -1,27 +1,31 @@
 interface Calculable{
+	double PI=3.14;
 	int sum(int v1,int v2);
 }
 
-class RealCal implements Calculable{
+interface Printable{
+	void print();
+}
+
+class RealCal implements Calculable, Printable{
 	@Override
 	public int sum(int v1, int v2) {
-		// TODO Auto-generated method stub
 		return v1+v2;
+	}
+
+	@Override
+	public void print() {
+		System.out.println("This is RealCal!!!");
 	}
 	
 }
-class DummyCal implements Calculable{ //fake class
-	public int sum(int v1, int v2) {
-		return 3;
-	}
-}
 
 public class InterfaceApp {
-
 	public static void main(String[] args) {
-		// 더하기 기능이 있는 클래스를 만들어주세요.
 		RealCal cal=new RealCal();
 		System.out.println(cal.sum(2, 1));
+		cal.print();
+		System.out.println(cal.PI);
 	}
 
 }
