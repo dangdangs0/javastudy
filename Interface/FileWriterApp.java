507a24cc272b59ac8ff9e231185d83ba44fc506a
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-24 12:45:06 +0900

    Create FileWriterApp.java

---
+++ Interface/FileWriterApp.java
@@ -0,0 +1,18 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+public class FileWriterApp {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Writer fileWriter=new FileWriter("filewriter.txt");
+		
+		fileWriter.write("data 1");
+		fileWriter.write("data 2");
+		fileWriter.write("data 3");
+		
+		fileWriter.close();
+	}
+
+}
